d9a1f7277600f07e7ad09ebae124f9921018135f
liangzhirong <liangzhirong@yy.com>
2021-08-03 11:00:53 +0800

    chore: release

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 module.exports = {
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        // releaseRules: [{ type: '/(feat|fix)/', release: 'patch' }]
+      }
+    ],
     '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
     '@semantic-release/npm',
+    '@semantic-release/git',
     '@semantic-release/gitlab',
-  ],
-};
+    'semantic-release-wxwork'
+  ]
+}
